c102d4c58ed9489d728ff40418cfe409121ea4d3
corvus96 <guillermoraven96@gmail.com>
2024-04-17 23:51:05 -0400

    Add pd controller just position part

--- mujoco-python/diff_car.py
+++ mujoco-python/diff_car.py
@@ -1,10 +1,12 @@
 
+import math
 import mujoco 
 import numpy as np
 import time
 from mujoco import viewer
 from dotenv import load_dotenv
 import os
+from kinematics.diff_mobile_control import Car
 
 # Load variables from .env file
 load_dotenv()
@@ -14,13 +16,14 @@ pc_dir = os.getenv("PC_FOLDER")
 
 def control_inv():
      # simulate time
-    simu_time = 5
+    simu_time = 40
     path = pc_dir + "/mujoco-python/diff_car.xml"
     model = mujoco.MjModel.from_xml_path(path)
     data = mujoco.MjData(model)
-
-    print(data.xpos)
-
+    # Get the car id
+    body_id = model.body("car").id
+    goal = [1, 1]
+    car = Car(model, data, 0.03, 0.06)
     with mujoco.viewer.launch_passive(model, data, show_left_ui=False,
       show_right_ui=False,) as viewer:
 
@@ -31,7 +34,8 @@ def control_inv():
       start = time.time()
       while viewer.is_running() and time.time() - start < simu_time:
         step_start = time.time()
-
+        car.compute_pose(goal, body_id, 100, 0.2)
+        print("actual position ({},{})".format(data.body(body_id).xpos[0],data.body(body_id).xpos[1]))
         # Pick up changes to the physics state, apply perturbations, update options from GUI.
         viewer.sync()
 
---
+++ mujoco-python/kinematics/diff_mobile_control.py
@@ -0,0 +1,51 @@
+import math
+from pickle import TRUE
+import mujoco 
+import numpy as np
+
+class Car:
+    def __init__(self, model, data, wheel_radius, robot_base):
+        self.model = model
+        self.data = data
+        self.wheel_radius = wheel_radius
+        self.robot_base = robot_base
+        self.prev_error = [0.0, 0.0] # index 0 = velocity error and index 1 = omega_error 
+    
+    def check_limits(self):
+        pass
+    def update_state(self):
+        # Get current state variables
+        omega_r = self.data.sensordata[0]
+        omega_l = self.data.sensordata[1]
+        theta = self.data.qpos[6]
+        # Update state
+        x_prime = (self.wheel_radius / 2.0) * np.cos(theta) * (omega_r + omega_l)
+        y_prime = (self.wheel_radius / 2.0) * np.sin(theta) * (omega_r + omega_l)
+        omega =  (omega_r - omega_l) * (self.wheel_radius / self.robot_base)
+        return x_prime, y_prime, theta, omega
+
+    def pd_controller(self, e, e_prime, kp, kd):
+        return kp * e + kd * e_prime
+
+    def compute_pose(self,goal, body_id, kp, kd, threshold=0.2):
+        linear_ctrl = 1
+        condition = 99999
+        if condition >= threshold:
+            # position control
+            x = self.data.body(body_id).xpos[0]
+            y = self.data.body(body_id).xpos[1]
+            x_prime, y_prime, theta, omega = self.update_state()
+            theta_d = math.atan2(goal[1] - y, goal[0] - x)
+            e_theta = theta_d - theta
+            omega_d = ((x - goal[0]) * y_prime - (y - goal[1]) * x_prime) / ((x - goal[0])**2 + (y - goal[1])**2)
+            e_omega = omega_d - omega
+            w_ctrl = self.pd_controller(e_theta, e_omega, kp, kd)
+            self.data.ctrl[0] = linear_ctrl - w_ctrl
+            self.data.ctrl[1] = linear_ctrl + w_ctrl
+            mujoco.mj_step(self.model, self.data)
+            condition = np.sqrt((x - goal[0])**2 + (y - goal[1])**2)
+            #orientation control
+
+
+
+
